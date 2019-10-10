39a0e19a7db8487d82df8f188364cb6619dd7e76
Rafael Mourao <rafael.mcf@hotmail.com>
2019-10-10 20:32:17 -0300

    Modificado o arquivo HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+//Observação: este arquivo foi CRIADO no site, mas foi todo escrito no repositório local criado no evento Hacktoberfest 2019 (PUC Minas).
+
+int main(){
+	printf("\t\t\tHello World!\n");
+	return 0;
+}
